59da1d010ecbcd071621f32251e133ddeea7a0cc
ZephonMakale <mzephon@gmail.com>
2020-11-16 06:41:20 +0300

    add location icon

--- src/app/nav-bar/nav-bar.component.ts
+++ src/app/nav-bar/nav-bar.component.ts
@@ -1,4 +1,4 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, Output, EventEmitter } from '@angular/core';
 
 @Component({
   selector: 'app-nav-bar',
@@ -6,6 +6,12 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./nav-bar.component.css']
 })
 export class NavBarComponent implements OnInit {
+username: string;
+@Output() mySearch = new EventEmitter<any> ();
+// tslint:disable-next-line:typedef
+search() {
+  this.mySearch.emit(this.username);
+}
 
   constructor() { }
 
